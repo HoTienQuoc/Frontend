f2427c12a7ce3f17bbf6fbd777fa876885a51d71
HoTienQuoc <113992899+HoTienQuoc@users.noreply.github.com>
2025-03-16 19:42:40 +0700

    afds

--- bai1/js/index.js
+++ bai1/js/index.js
@@ -1,3 +1,4 @@
+//toggle Background active
 const bannerNavigator = name => {
     let items = document.querySelectorAll('.item');
     let descriptions = document.querySelectorAll('.description');
@@ -14,3 +15,28 @@ const bannerNavigator = name => {
         }
     });
 }
+
+// switch Background
+window.addEventListener('load', ()=>{
+    const buttons = document.querySelectorAll('.name');
+    const close = document.querySelector('.close');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function (e){
+            e.preventDefault();
+            buttons.forEach(el => {
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+            close.classList.add('active');
+            bannerNavigator(this.getAttribute('data-target'));
+        });
+    });
+    close.addEventListener('click', function (e){
+        e.preventDefault();
+        close.classList.remove('active');
+        buttons.forEach(el => {
+            el.classList.remove('active');
+        });
+        bannerNavigator(this.getAttribute('data-target'));
+    });
+})
